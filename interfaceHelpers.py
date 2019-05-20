8e37db483254da619067a64d5824ed0cf8b4a9f7
landjbs <landonsmith@college.harvard.edu>
2019-05-20 17:36:54 -0400

    search module init

---
+++ interfaceHelpers.py
@@ -0,0 +1,4 @@
+def homepage(new=True):
+    print(f"{'-'*80}\n\t\tWelcome to Landon Search!\n{'-'*80}")
+    print("\t1 - Search\n\t2 - Add new page")
+    userResponse = input("")
---
+++ searchmodule.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char main(void) {
+  int x = 5;
+  int y = 6;
+  int z = x + y;
+  printf("Number: %i\n", z);
+}
