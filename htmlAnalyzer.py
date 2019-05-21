63ff4fd94336825fbc5a672c95bc142bc49f2dd3
landjbs <landonsmith@college.harvard.edu>
2019-05-21 19:21:12 -0400

    txt_to_string

--- htmlAnalyzer.py
+++ htmlAnalyzer.py
@@ -1,1 +1,16 @@
 import re
+
+def txt_to_string(pageText):
+    """ Converts txt file to string """
+    with open(pageText, 'r') as FileObj:
+        pageString = ''
+        for line in FileObj:
+            assert (isinstance(line, str)), ".txt lines must have type str"
+            pageString += line
+        return pageString
+
+# def find_meta(pageString):
+#     """ Returns the metadata of a page as a dict """
+
+
+# find_meta('samplePage.txt')
