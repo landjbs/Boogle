ecc8205b3bbf5bff2ddc4e853c2c1f33fb0c900e
landjbs <landonsmith@college.harvard.edu>
2019-05-21 16:21:34 -0400

    search module updates

--- searchmodule.c
+++ searchmodule.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  int x[1] = 5;
-  printf("%d\n", x);
+int max(void) {
+  // allocate ints for readin
+  int num1;
+  int num2;
+  // read in input
+  scanf("X: %d", num1);
+  scanf("Y: %d", num2);
+  // initialize result int
+  int result;
+  // compare and return
+  if (num1 > num2)
+    result = num1;
+  else
+    result = num2;
+  return result;
+}
+
+int mario(int height) {
+  for 
+}
+
+char last(char c1, char c2) {
+  char result;
+
+  int a1 = c1;
+  int a2 = c2;
+
+  if (a1 > a2)
+    result = c1;
+  else
+    result = c2;
+  return result;
+}
+
+int main() {
+  int mario();
 }
---
+++ sleeper.py
@@ -0,0 +1,24 @@
+import time
+import numpy as np
+
+def gen_string():
+    # create uniform dist of ints across ascii hexes
+    raw_ints = np.random.randint(33,127, 30)
+    # map hex to ascii and int chars
+    cList = list(map(lambda i : chr(i), raw_ints))
+    iList = list(map(lambda i : str(i), raw_ints))
+    # create strings for cList and i List
+    cOut = "".join(cList)
+    iOut = "".join(iList)
+    # create and return outstr
+    outStr = cOut + ": " + iOut
+    return outStr
+
+def gen_rand():
+    while True:
+        print(gen_string())
+        if np.random.randint(0,60)==30:
+            print('-'*40)
+        time.sleep(np.random.uniform(0.01, 0.5))
+
+gen_rand()
