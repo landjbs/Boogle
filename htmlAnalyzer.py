056779377fa308248499a4ad8a0de27dea393e5c
landjbs <landonsmith@college.harvard.edu>
2019-05-21 19:25:34 -0400

    sample changed find_meta prog

--- htmlAnalyzer.py
+++ htmlAnalyzer.py
@@ -3,14 +3,16 @@ import re
 def txt_to_string(pageText):
     """ Converts txt file to string """
     with open(pageText, 'r') as FileObj:
-        pageString = ''
-        for line in FileObj:
-            assert (isinstance(line, str)), ".txt lines must have type str"
-            pageString += line
+        # create string of joined lines in .txt file
+        pageString = "".join(line for line in FileObj)
         return pageString
 
-# def find_meta(pageString):
-#     """ Returns the metadata of a page as a dict """
+def find_meta(pageString):
+    """ Returns the metadata of a page as a dict """
+    x = re.findall("<meta" + ".+" + ">", pageString)
+    print(x)
 
 
-# find_meta('samplePage.txt')
+sample_pageString = txt_to_string('samplePage.txt')
+
+find_meta(sample_pageString)
