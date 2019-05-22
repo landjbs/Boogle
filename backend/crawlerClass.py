c989369919a4a5cb98d41fc05a4a9c2ba195d57e
landjbs <landonsmith@college.harvard.edu>
2019-05-22 16:53:24 -0400

    decode_line init

--- backend/crawlerClass.py
+++ backend/crawlerClass.py
@@ -1,6 +1,20 @@
-import urllib
+import urllib.request
 
 def scrape_url(url):
-    page = urllib.urlopen(url)
-    outstr = "".join([line for line in page])
+    """ Converts string of url link to string of page contents """
+    try:
+        page = urllib.request.urlopen(url)
+    except:
+        raise ValueError(f"Unable to access contents of '{url}''")
+
+    def decode_line(line):
+        """ Decodes and consolidates line of html """
+        decodedLine = (line).decode("utf-8")
+        return decodedLine
+
+    outstr = "".join([decode_line(line) for line in page])
     page.close()
+    print(outstr)
+
+
+scrape_url("https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping")
