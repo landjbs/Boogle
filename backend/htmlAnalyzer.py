92b266580dcee00408db5f16432d0dc6511ac324
landjbs <landonsmith@college.harvard.edu>
2019-05-23 00:11:25 -0400

    find_links init

--- backend/htmlAnalyzer.py
+++ backend/htmlAnalyzer.py
@@ -7,30 +7,33 @@ def txt_to_string(pageText):
         pageString = "".join(line for line in FileObj)
         return pageString
 
-def find_meta(pageString, paramList):
-    """
-    Args: string of decoded page text, list of parameters to grab
-    Returns: metadata of pageString as a dict mapping metaParams to values
-    """
-    # find contents of all meta tags in pageString
-    metaList = re.findall("(?<=<meta )" + ".+" + "(?=>)", pageString)
+# def find_meta(pageString, paramList):
+#     """
+#     Args: string of decoded page text, list of parameters to grab
+#     Returns: metadata of pageString as a dict mapping metaParams to values
+#     """
+#     # find contents of all meta tags in pageString
+#     metaList = re.findall("(?<=<meta )" + ".+" + "(?=>)", pageString)
+#
+#     def paramMatcher(elt, paramList=paramList):
+#         """ Matches metaParams in elt of metaList """
+#         param_matchString = f"{[paramList]}" + ".+" + "=" + ".+"
+#         paramList = re.findall(param_matchString, elt)
+#         return paramList
+#
+#     # find all individual params within metaList
+#     paramString = list(map(lambda elt : paramMatcher(elt), metaList))
+#
+#
+#     def meta_to_dict(metaList):
+#         """ Converts meta list to dict """
+#
+#
+# sample_pageString = txt_to_string('samplePage.txt')
+#
+# print(sample_pageString)
+#
+# find_meta(sample_pageString)
 
-    def paramMatcher(elt, paramList=paramList):
-        """ Matches metaParams in elt of metaList """
-        param_matchString = f"{[paramList]}" + ".+" + "=" + ".+"
-        paramList = re.findall(param_matchString, elt)
-        return paramList
-
-    # find all individual params within metaList
-    paramString = list(map(lambda elt : paramMatcher(elt), metaList))
-
-
-    def meta_to_dict(metaList):
-        """ Converts meta list to dict """
-
-
-sample_pageString = txt_to_string('samplePage.txt')
-
-print(sample_pageString)
-
-find_meta(sample_pageString)
+def find_links(pageString, paramList):
+    """ Find urls contained by all <a href=""> tags
