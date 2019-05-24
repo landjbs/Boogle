634e9638d9b7832969a79a13e9a335bcec808c6b
landjbs <landonsmith@college.harvard.edu>
2019-05-24 19:06:21 -0400

    widetable test

---
+++ backend/widetable.cpp
@@ -0,0 +1,16 @@
+class Test
+{
+    private:
+        int data1;
+        float data2;
+
+    public:
+        void function1()
+        {   data1 = 2;  }
+
+        float function2()
+        {
+            data2 = 3.5;
+            return data2;
+        }
+   };
