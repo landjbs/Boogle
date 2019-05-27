4f0575d6a9b163ef09f807a6edeb604ef19e698f
landjbs <landonsmith@college.harvard.edu>
2019-05-27 13:01:03 -0400

    dmozAnalyzer init

---
+++ backend/dmozAnalyzer.py
@@ -0,0 +1,16 @@
+import re
+import hmtlAnalyzer
+import crawlerClass
+import numpy as np
+import pandas as pd
+
+"""
+
+Features:
+
+"""
+
+dmozMatrix = np.read_tsv("")
+
+for url, bin in dmozMatrix:
+    pageString = crawlerClass.
