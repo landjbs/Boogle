a8d3b902d4033135702a5f4e9255b1bfc39e0d4c
landjbs <landonsmith@college.harvard.edu>
2019-05-28 19:19:46 -0400

    textVector init

--- backend/dmozAnalyzer.py
+++ backend/dmozAnalyzer.py
@@ -1,16 +1,66 @@
-import re
-import hmtlAnalyzer
-import crawlerClass
-import numpy as np
-import pandas as pd
+# import re
+# import pandas as pd
+#
+#
+# dmozDF = pd.read_csv("data/dmoz_domain_category.tab.tsv", sep="\t")
+#
+# def analyze_page(page):
+#     return page
+#
 
-"""
 
-Features:
+# Python program to generate word vectors using Word2Vec
 
-"""
+# importing all necessary modules
+from nltk.tokenize import sent_tokenize, word_tokenize
+import warnings
 
-dmozMatrix = np.read_csv("../outputs/dmoz_domain_category.tab.tsv", delimiter="\t")
+warnings.filterwarnings(action = 'ignore')
 
-for url, path in dmozMatrix:
-    pageString = crawlerClass.url_to_string(url)
+import gensim
+from gensim.models import Word2Vec
+
+#  Reads ‘alice.txt’ file
+sample = open("C:\\Users\\Admin\\Desktop\\alice.txt", "r")
+s = sample.read()
+
+# Replaces escape character with space
+f = s.replace("\n", " ")
+
+data = []
+
+# iterate through each sentence in the file
+for i in sent_tokenize(f):
+    temp = []
+
+    # tokenize the sentence into words
+    for j in word_tokenize(i):
+        temp.append(j.lower())
+
+    data.append(temp)
+
+# Create CBOW model
+model1 = gensim.models.Word2Vec(data, min_count = 1,
+                              size = 100, window = 5)
+
+# Print results
+print("Cosine similarity between 'alice' " +
+               "and 'wonderland' - CBOW : ",
+    model1.similarity('alice', 'wonderland'))
+
+print("Cosine similarity between 'alice' " +
+                 "and 'machines' - CBOW : ",
+      model1.similarity('alice', 'machines'))
+
+# Create Skip Gram model 
+model2 = gensim.models.Word2Vec(data, min_count = 1, size = 100,
+                                             window = 5, sg = 1)
+
+# Print results
+print("Cosine similarity between 'alice' " +
+          "and 'wonderland' - Skip Gram : ",
+    model2.similarity('alice', 'wonderland'))
+
+print("Cosine similarity between 'alice' " +
+            "and 'machines' - Skip Gram : ",
+      model2.similarity('alice', 'machines'))
---
+++ backend/textVectorizer.py
@@ -0,0 +1,12 @@
+# Library to vectorize text using Doc2Vec model
+# Inspired by https://cs.stanford.edu/~quocle/paragraph_vector.pdf,
+# https://india.endurance.com/machine-learning-website-categorization/,
+# https://towardsdatascience.com/word2vec-from-scratch-with-numpy-8786ddd49e72
+
+import re
+
+
+def tokenize(text):
+    """ Convert string to tokens with at least 1 letter """
+    tokenMatch = re.compile(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
+    return tokenMatch.findall(text.lower())
