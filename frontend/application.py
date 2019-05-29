ef82c3f2782800beb5238f3bfb3308edeb931773
landjbs <landonsmith@college.harvard.edu>
2019-05-29 16:09:56 -0400

    backend restructure

--- frontend/application.py
+++ frontend/application.py
@@ -5,7 +5,4 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method=='GET':
-        return render_template('templates/index.html')
-    else:
-        query = request.form.get('home-search')
-        return(query)
+        return render_template('index.html')
