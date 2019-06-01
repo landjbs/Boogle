f55a6b442350c897440249c513f8b3542f1d9fde
landjbs <landonsmith@college.harvard.edu>
2019-05-31 21:35:28 -0400

    scape_domz_line pageSTring and pageText

--- backend/dmozAnalyzer.py
+++ backend/dmozAnalyzer.py
@@ -7,13 +7,15 @@ import crawlers.htmlAnalyzer as ha
 dmozDF = pd.read_csv("data/test.tab.tsv", sep="\t", names=["url", "path"])
 
 def scrape_dmoz_line(line):
-    """ Converts line of dmoz dataframe to """
+    """ Converts line of dmoz dataframe to tuple of pageText and top folder """
 
 def scrape_dmoz(df):
     """ Scrapes dmoz dataframe of urls and folders to return dataframe of
     readable pageText"""
 
     for index, row in df.iterrows():
-        print(row['url'])
+        row['url'] = 0
 
 scrape_dmoz(dmozDF)
+
+print(dmozDF)
