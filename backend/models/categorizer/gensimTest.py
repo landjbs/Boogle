2ab8d8da8f7a1b4b82bb841de9210b4e02fde008
landjbs <landonsmith@college.harvard.edu>
2019-06-03 18:22:28 -0400

    train_d2v imp

--- backend/models/categorizer/gensimTest.py
+++ backend/models/categorizer/gensimTest.py
@@ -5,52 +5,67 @@ nltk.download('punkt')
 
 data = ["peach pie", "berry cake", "small man"]
 
-# list mapping list of document tokens to unique integer tag
-tagged_data = [TaggedDocument(words=nltk.tokenize.word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
+def train_d2v(data, path='docVec.model', max_epochs=100, vec_size=20, alpha=0.025):
+    """ Trains doc vectorization model on iterable of doc and saves model to path """
+    # list mapping list of document tokens to unique integer tag
+    tagged_data = [TaggedDocument(words=nltk.tokenize.word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
 
-# set hyperparams for model training
-max_epochs = 100
-vec_size = 20
-alpha = 0.025
+    # initialize model
+    model = Doc2Vec(vector_size=vec_size,
+                    alpha=alpha,
+                    min_alpha=0.00025,
+                    min_count=1,
+                    dm =1)
 
-# initialize model
-model = Doc2Vec(vector_size=vec_size,
-                alpha=alpha,
-                min_alpha=0.00025,
-                min_count=1,
-                dm =1)
+    # build vector of all words contained in tagged data
+    model.build_vocab(tagged_data)
 
-# build vector of all words contained in tagged data
-model.build_vocab(tagged_data)
+    # train model for max_epochs
+    for epoch in range(max_epochs):
+        print(f'\tTraining: {epoch}', end="\r")
+        model.train(tagged_data,
+                    # signify that tagged_data is what was used to build vocab
+                    total_examples=model.corpus_count,
+                    # signify that train is only called once for efficiency
+                    epochs=model.iter)
+        # decrease the learning rate
+        model.alpha -= 0.0002
+        # fix the learning rate, no decay
+        model.min_alpha = model.alpha
 
-# train model for max_epochs
-for epoch in range(max_epochs):
-    print(f'\tIteration {epoch}', end="\r")
-    model.train(tagged_data,
-                # signify that tagged_data is what was used to build vocab
-                total_examples=model.corpus_count,
-                # signify that train is only called once for efficiency
-                epochs=model.iter)
-    # decrease the learning rate
-    model.alpha -= 0.0002
-    # fix the learning rate, no decay
-    model.min_alpha = model.alpha
-#
-# model.save("d2v.model")
-# print("Model Saved")
-#
-# from gensim.models.doc2vec import Doc2Vec
-#
-# model= Doc2Vec.load("d2v.model")
-# #to find the vector of a document which is not in training data
-# test_data = nltk.tokenize.word_tokenize("apple pie".lower())
-# v1 = model.infer_vector(test_data)
-# # print("V1_infer", v1)
-#
-# # to find most similar doc using tags
-# similar_doc = model.docvecs.most_similar('0')
-# print(similar_doc)
-#
-#
-# # to find vector of doc in training data using tags or in other words, printing the vector of document at index 1 in training data
-# # print(model.docvecs['1'])
+    # save model to path
+    model.save(path)
+    print(f"Model saved to {path}.")
+
+
+
+
+def vectorize_document(doc, model):
+
+    #
+    #
+    # model= Doc2Vec.load("d2v.model")
+    # # to find the vector of a document which is not in training data
+    # test_data = nltk.tokenize.word_tokenize("apple pie".lower())
+    # v1 = model.infer_vector(test_data)
+    # # # print("V1_infer", v1)
+    # #
+    # # # to find most similar doc using tags
+    # # similar_doc = model.docvecs.most_similar('0')
+    # # print(similar_doc)
+    # #
+    # #
+    # # # to find vector of doc in training data using tags or in other words, printing the vector of document at index 1 in training data
+    # # # print(model.docvecs['1'])
+
+
+
+
+
+
+
+
+
+
+
+pass
