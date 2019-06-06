d5bc3e73c7f387d26bd34ce38faf48db42b8df68
landjbs <landonsmith@college.harvard.edu>
2019-06-06 16:40:03 -0400

    sorting method updated

--- backend/dataStructures/thicctable.py
+++ backend/dataStructures/thicctable.py
@@ -4,64 +4,84 @@ class Thicctable():
     """
     Class to store indexed webdata in two branches:
         -Knowledge Branch: Maps knowledge tokens to indexed, relevant list
-        -Concept Branch: Maps page clusters to indexed cluster contents
+        -Concept Branch: Maps page clusters to indexed list of cluster contents
     Methods:
-        -Insert: Add a webpage to a branch
+        -Insert: Add value to list associated with
     """
 
     def __init__(self, knowledgeKeys=[], conceptKeys=[]):
-        """ Initialize both branches as stores mapping keys to empy lists """
+        """ Initialize both branches as stores mapping keys to empty lists """
         self.knowledgeBranch = {key:[] for key in knowledgeKeys}
         self.conceptBranch = {key:[] for key in conceptKeys}
 
-    def insert(self, key, value, knowledge):
-        """ """
-        if knowledge:
-            self.knowledgeBranch[key].append(value)
-        else:
-            self.conceptBranch[key].append(value)
+    def knowledge_insert(self, key, (score, info)):
+        """ Add value to key in knowledgeBranch"""
+        self.knowledgeBranch[key].append((score, info))
 
-    def metrics(self, branch):
-        """ Display metrics """
-        branch = branch.lower()
+    def knowledge_insert(self, key, (score, info)):
+        """ Add value to key in conceptBranch """
+        self.conceptBranch[key].append((score, info))
+
+    def sort_branch(self, branch, rankAlgo):
+        """ Applies page ranking algorithm to rank pages for every key in branch """
         # assert valid branch
         assert (branch in ['knowledge', 'concept']), "Valid branches are 'knowledge' or 'concept'"
-        # query branch metrics
+        # define root
         root = self.knowledgeBranch if (branch=='knowledge') else self.conceptBranch
+        # apply ranking algorithm to every value in key, value store
+        rankLambda = lambda key : rankAlgo(key, root[key])
+        root = dict(map(rankLambda, root))
+        # store ranked root in knowledge or conceptBranch
+        if (branch=='knowledge'):
+            self.knowledgeBranch = root
+        else:
+            self.conceptBranch = root
+
+    def search(self, branch, key, numPages=20):
+        """
+        Pops top numPages pages from valueList mapped from key in branch
+        Built for speed, not readability
+        """
+        if branch=="knowledge":
+            return self.knowledgeBranch[key][:numPages]
+        elif branch=="concept":
+            return self.conceptBranch[key][:numPages]
+        else:
+            raise ValueError("Invalid Branch")
+
+    def metrics(self, branch):
+        """ Display metrics for every page in a branch """
+        # assert valid branch
+        assert (branch in ['knowledge', 'concept']), "Valid branches are 'knowledge' or 'concept'"
+        # define root
+        root = self.knowledgeBranch if (branch=='knowledge') else self.conceptBranch
+        # query branch metrics
         for key in root:
             print(f"Key: {key}\n\tNumber of Pages: {len(root[key])}")
         return True
 
 # Testing
 
-import time
 import numpy as np
-
-
-loadStart = time.time()
 
 knowledgeSet = load("/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeTokens.set")
 
-loadEnd = time.time()
-
-print(f"Loaded in {loadEnd - loadStart} seconds")
-
-createStart = time.time()
+print("Loaded")
 
 x = Thicctable(knowledgeKeys=knowledgeSet)
 
-createEnd = time.time()
+print("x init")
 
-print(f"Created in {createEnd - createStart} seconds")
+for _ in range(10000000):
+    key = 'harvard'
+    value = np.random.choice([0, 1, 2], size=1)
+    x.insert(key, value[0], True)
 
-addStart = time.time()
+print('added')
 
-for _ in range(10000):
-    key = np.random.choice(['hi', 'hello', 'yo'], size=1)
-    x.insert(key[0], 'html', True)
-
-addEnd = time.time()
-
-print(f"Added in {addEnd - addStart} seconds")
-
-x.metrics('knowledge')
+import time
+start = time.time()
+result = x.search('knowledge', 'hi')
+end = time.time()
+print(f"Queried in: {end - start}")
+print(result)
