c27301054040733058f403f924e7a180adf268c9
landjbs <landonsmith@college.harvard.edu>
2019-06-07 10:31:09 -0400

    clean_knowledge_token rearranged

--- backend/models/knowledgeClassifier/knowledgeTokenizer.py
+++ backend/models/knowledgeClassifier/knowledgeTokenizer.py
@@ -5,23 +5,23 @@ sys.path.append(os.path.abspath(os.path.join('..', '..', 'dataStructures')))
 from objectSaver import save, load
 
 # matcher for elements to replace with "" in rawToken
-stripString = '[(|)|.|!|?|\[|\]|\{|\}|\n|=|$|*|+|"|' + r"\\" + "|']"
+stripString = '[(|)|.|!|?|,|\[|\]|\/|\{|\}|\n|=|$|*|+|"' + r".\\" + "|']"
 stripMatcher = re.compile(stripString)
 
 # matcher for elements to replace with "_" in rawToken
-conversionString = "[-| ]"
-conversionMatcher = re.compile(conversionString)
+# conversionString = "[-| ]"
+# conversionMatcher = re.compile(conversionString)
 
 
 def clean_knowledge_token(rawToken):
     """ Cleans rawToken by stripping parentheses and replacing _ with spaces """
-    # replace stripMatcher with "" in rawToken
-    cleanToken = re.sub(stripMatcher, "", rawToken)
-    # replace conversionMatcher with "_" in in cleanToken
-    convertedToken = re.sub(conversionMatcher, "_", cleanToken)
+    print(rawToken)
     # lowercase token
-    lowerToken = convertedToken.lower()
-    print(f"Before: {rawToken} | After: {lowerToken}")
+    lowerToken = rawToken.lower()
+    # replace stripMatcher with "" in lowerToken
+    cleanToken = re.sub(stripMatcher, "", rawToken)
+    if len(re.split('\W', rawToken)) > 5:
+        print(lowerToken)
     return lowerToken
 
 
@@ -40,7 +40,8 @@ def build_knowledgeSet(knowledgeFile, outPath=""):
 
 def build_knowledgeMatcher(knowledgeSet, outPath=""):
     """ Builds regex matcher for words in knowledgeSet """
-    knowledgeString = "(" + "|".join(knowledgeSet) + ")"
+    knowledgeString = "(?" + "|".join(knowledgeSet) + ")"
+    print(knowledgeString)
     knowledgeMatcher = re.compile(knowledgeString)
     if not (outPath==""):
         save(knowledgeMatcher, outPath)
@@ -52,32 +53,30 @@ def knowledgeTokenize_search(inStr, knowledgeSet):
         return inStr
 
 
-# print(clean_knowledge_token("1\2Python-docs\n"))
-
+print(clean_knowledge_token(''))
 
 # knowledgeSet = build_knowledgeSet("enwiki-latest-all-titles-in-ns0",
 #                     outPath="/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeTokens.set")
 
 # print('Built')
 
-knowledgeSet = load('/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeTokens.set')
+# knowledgeSet = load('/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeTokens.set')
 
 # knowledgeMatcher = build_knowledgeMatcher(knowledgeSet[:2000], outPath="/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeMatcher.re")
 # print("Created")
 #
 
-knowledgeMatcher = build_knowledgeMatcher([clean_knowledge_token(token) for token in ["hi", "hi college","h", "hyo"]])
-
-print(knowledgeMatcher)
-# knowledgeMatcher = load('/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeMatcher.re')
 
 # print(knowledgeMatcher)
+# # knowledgeMatcher = load('/Users/landonsmith/Desktop/DESKTOP/Code/personal-projects/search-engine/backend/data/outData/knowledgeMatcher.re')
 #
-while True:
-    test = input("Search: ")
-    test = clean_knowledge_token(test)
-    print(re.findall(knowledgeMatcher, test))
-
+# # print(knowledgeMatcher)
+# #
+# while True:
+#     test = input("Search: ")
+#     test = clean_knowledge_token(test)
+#     print(re.findall(knowledgeMatcher, test))
+#
 
 
 
