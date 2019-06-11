0eea6a42074cbf9053d875236928aa9bfe535724
landjbs <landonsmith@college.harvard.edu>
2019-06-11 15:36:02 -0400

    scrapeWikiWords removed

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,7 @@
 import models.knowledge.knowledgeTokenizer as knowledgeTokenizer
 
-knowledgeSet = knowledgeTokenizer.build_knowledgeSet('data/inData/wikiTitles.txt')
+knowledgeSet = knowledgeTokenizer.build_knowledgeSet('data/inData/wikiTitles.txt',
+                                                    "data/outData/knowledgeSet.sav")
 
 while True:
     text = input("Search: ")
