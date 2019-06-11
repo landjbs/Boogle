559cdbe8b8053caf1031354a02487bdfc8daa37a
landjbs <landonsmith@college.harvard.edu>
2019-06-11 16:27:17 -0400

    find_tokens weight added

--- backend/models/knowledge/knowledgeFinder.py
+++ backend/models/knowledge/knowledgeFinder.py
@@ -9,21 +9,23 @@ from flashtext import KeywordProcessor
 knowledgeProcessor = KeywordProcessor(case_sensitive=False)
 knowledgeProcessor.add_keyword('foo')
 
-def find_tokens(pageText, knowledgeProcessor):
-    """
-    Returns dict mapping knowledge tokens found in text to number of occurences
-    """
-    # use knowledgeProcessor to extract tokens from page text
-    keywordsFound = knowledgeProcessor.extract_keywords(pageText)
-    # create dict mapping keywords to number of times used in pageText using re.findall()
-    keywordDict = {keyword:(len(re.findall(keyword, pageText, re.IGNORECASE))) for keyword in keywordsFound}
-    return keywordDict
 
 # def score_token(token, div):
 #     """
 #     Args: single knowledge token and html division where it occurred
 #     Returns: score of token weighted by
 #     """
+
+
+def find_tokens(pageText, knowledgeProcessor):
+    """
+    Returns dict mapping knowledge tokens to score assigned by score_token
+    """
+    # use knowledgeProcessor to extract tokens from page text
+    keywordsFound = knowledgeProcessor.extract_keywords(pageText)
+    # create dict mapping keywords to number of times used in pageText using re.findall()
+    keywordDict = {keyword:(len(re.findall(keyword, pageText, re.IGNORECASE))) for keyword in keywordsFound}
+    return keywordDict
 
 
 def find_weighted_knowledge(divDict):
@@ -38,7 +40,13 @@ def find_weighted_knowledge(divDict):
     Sample input: {'title':'foo bar', 'h1':'foo', 'p':'hello world'}
     """
     for div in divDict:
+        divText = divDict[div]
+        divLen = len(divText.split())
+        weightedTokens =
 
+        # tokenDict = (find_tokens(divDict[div], knowledgeProcessor))
+        # tokenDict = dict(map(lambda k : (k[0], k[1]/2), tokenDict.items()))
+        # print(tokenDict)
 
 
 
