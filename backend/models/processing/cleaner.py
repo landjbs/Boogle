39088c806d78a6458c1560e77d24973f25a1033d
landjbs <landonsmith@college.harvard.edu>
2019-06-12 01:17:40 -0400

    end_test imp

--- backend/models/processing/cleaner.py
+++ backend/models/processing/cleaner.py
@@ -17,7 +17,7 @@ import re
 ## Matchers ##
 # matches things that look like a single html tag
 tagMatcher = re.compile(r"<[^\s][^<]*>")
-# matches non-alpha or space characters (dash must be at end)
+# matches non-alpha, space, or sentence-ending punctuation (dash must be at end)
 stripMatcher = re.compile(r"[^a-zA-Z\s\t\n_-]")
 # matches any sequence of tabs, newlines, spaces, underscores, and dashes
 spaceMatcher = re.compile(r"[\t|\n|\s|-|_]+")
@@ -39,13 +39,13 @@ def clean_text(rawString):
     return loweredString
 
 def end_test(rawString):
-    endMatcher = r"(?<=[a-zA-z])(?P<punc>[.!?])(?=\s[A-Z])"
+    """ Adds space before sentence-ending punctuation. Not yet used. """
     return re.sub(r"(?<=[a-zA-z])(?P<punc>[.!?])(?=\s[A-Z])", " \g<punc>", rawString)
 
 
 while True:
     test = input("Test: ")
-    print(end_test(test))
+    print(clean_text(test))
 
 
 
