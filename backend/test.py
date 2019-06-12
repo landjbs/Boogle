bebd1b6e11d1d3cd2f46505a2081b87e26d4c33a
landjbs <landonsmith@college.harvard.edu>
2019-06-11 20:19:07 -0400

    test knowledgeProcessor imp

--- backend/test.py
+++ backend/test.py
@@ -1,18 +1,12 @@
 import models.knowledge.knowledgeBuilder as knowledgeBuilder
 from dataStructures.objectSaver import load
-import crawlers.htmlAnalyzer as ha
-import models.processing.cleaner as cleaner
 
-while True:
-    url = input("Search: ")
-    try:
-        print(ha.get_pageText(url))
-    except Exception as e:
-        print(e)
+knowledgeSet = load('data/outData/knowledge/knowledgeSet.sav')
 
+print("Loaded!")
 
-# knowledgeProcessor = load('data/outData/knowledge/knowledgeProcessor.sav')
-#
-# test = knowledgeBuilder.build_freqDict("data/outData/dmozProcessed/Arts", knowledgeProcessor, 'data/outData/knowledge/freqDict.sav')
-#
-# print(test['harvard'])
+knowledgeProcessor = knowledgeBuilder.build_knowledgeProcessor(knowledgeSet, 'data/outData/knowledge/knowledgeProcessor.sav')
+
+test = knowledgeBuilder.build_freqDict("data/outData/dmozProcessed/Arts", knowledgeProcessor, 'data/outData/knowledge/freqDict.sav')
+
+print(test['harvard'])
