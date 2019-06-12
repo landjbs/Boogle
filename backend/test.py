a7a6e4138363817d13d40393e60a72ce97fae99b
landjbs <landonsmith@college.harvard.edu>
2019-06-11 22:25:49 -0400

    test load updates

--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,7 @@
 import models.knowledge.knowledgeBuilder as knowledgeBuilder
 from dataStructures.objectSaver import load
 
-knowledgeSet = load('data/outData/knowledge/knowledgeSet.sav')
-
-print("Loaded!")
-
-knowledgeProcessor = knowledgeBuilder.build_knowledgeProcessor(knowledgeSet, 'data/outData/knowledge/knowledgeProcessor.sav')
+knowledgeProcessor = load('data/outData/knowledge/knowledgeProcessor.sav')
 
 test = knowledgeBuilder.build_freqDict("data/outData/dmozProcessed/Arts", knowledgeProcessor, 'data/outData/knowledge/freqDict.sav')
 
