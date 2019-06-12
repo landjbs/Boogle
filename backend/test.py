82daae2afd27efc21dd5f7660f3e8a78517ec4da
landjbs <landonsmith@college.harvard.edu>
2019-06-12 01:08:40 -0400

    test rework

--- backend/test.py
+++ backend/test.py
@@ -2,17 +2,14 @@ import models.knowledge.knowledgeFinder as knowledgeFinder
 from dataStructures.objectSaver import load
 
 
-# knowledgeProcessor = load('data/outData/knowledge/knowledgeProcessor.sav')
-# print("Done1")
+knowledgeProcessor = load('data/outData/knowledge/knowledgeProcessor.sav')
 freqDict = load('data/outData/knowledge/freqDict.sav')
 
-print(freqDict['art'])
-
-# while True:
-    # div = input("Div: ")
-    # text = input("Text: ")
-    # try:
-    #     divDict = {div:text}
-    #     print(knowledgeFinder.score_divDict(divDict, knowledgeProcessor, freqDict))
-    # except Exception as e:
-    #     print(e)
+while True:
+    div = input("Div: ")
+    text = input("Text: ")
+    try:
+        divDict = {div:text}
+        print(knowledgeFinder.score_divDict(divDict, knowledgeProcessor, freqDict))
+    except Exception as e:
+        print(e)
