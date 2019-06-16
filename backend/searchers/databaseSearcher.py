2462099a61fa05722ad070360f1891a4e0883cae
landjbs <landonsmith@college.harvard.edu>
2019-06-16 14:50:38 -0400

    search_database v1 imp

--- backend/searchers/databaseSearcher.py
+++ backend/searchers/databaseSearcher.py
@@ -4,7 +4,7 @@
 from searchers.searchTokenizer import search_tokens
 from models.ranking.pageRankers import score_intersection
 
-def search_database(rawSearch, database):
+def search_database(rawSearch, database, n=20):
     """
     Parses search and finds results in database.
     Supports multitoken search.
@@ -14,24 +14,30 @@ def search_database(rawSearch, database):
     searchTokens = get_search_tokens(rawSearch)
     # if only one search token is used, simply search the bucket
     if (len(searchTokens)==1):
-        finalResults = database.search_index()
-    # query database for n results for each searchToken
-    unfilteredResults = [database.search_full(token, n=10000) for token in searchTokens]
-    # intialize list to hold newly scored pages
-    filteredResults = []
-    # iterate over results for first token
-    for result in unfilteredResults[0]:
-        # score result for all search tokens
-        knowledgeTokens = result[2]
-        # list to hold scores for all searchTokens in current result
-        try:
-            scoreList = [knowledgeTokens[token] for token in searchTokens]
-            # get score of page based on scoreList and load time
-            pageScore = score_intersection(scoreList, result[4])
-            # add tuple of page url, title, and score to filteredResults
-            filteredResults.append((result[0], result[1], pageScore))
-        # skip page if it doesn't have a score for any one of the search tokens
-        except:
-            pass
-    # sort filtered results
-    sortedResults
+        finalResults = database.search_index(key=searchTokens[0],
+                                            indexLambda=(lambda result : result[:2]),
+                                            n=n)            
+    else:
+        # query database for n results for each searchToken
+        unfilteredResults = [database.search_full(token, n=10000) for token in searchTokens]
+        # intialize list to hold newly scored pages
+        filteredResults = []
+        # iterate over results for first token
+        for result in unfilteredResults[0]:
+            # score result for all search tokens
+            knowledgeTokens = result[2]
+            # list to hold scores for all searchTokens in current result
+            try:
+                scoreList = [knowledgeTokens[token] for token in searchTokens]
+                # get score of page based on scoreList and load time
+                pageScore = score_intersection(scoreList, result[4])
+                # add tuple of page url, title, and score to filteredResults
+                filteredResults.append((result[0], result[1], pageScore))
+            # skip page if it doesn't have a score for any one of the search tokens
+            except:
+                pass
+            # sort filtered results based on lat elt of each result (the score)
+            sortedResults = filteredResults.sort(cmp=(lambda result:result[-1]), reverse=True)
+            # final results is list of url and title of sorted pages
+            finalResults = [result[:2] for i, result in enumerate(sortedResults) if i<n]
+    return finalResults
