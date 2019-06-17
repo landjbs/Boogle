3b8862dab41dce7340a676851b559517bbfa069a
landjbs <landonsmith@college.harvard.edu>
2019-06-17 18:10:14 -0400

    rangedString fix

--- backend/models/processing/cleaner.py
+++ backend/models/processing/cleaner.py
@@ -18,7 +18,7 @@ import re
 # matches things that look like a single html tag
 tagMatcher = re.compile(r"<[^\s][^<]*>")
 # matches non-alpha, space, or sentence-ending punctuation (dash must be at end)
-stripMatcher = re.compile(r"[^0-9a-zA-Z\s\t\n_]")
+stripMatcher = re.compile(r"[^0-9a-zA-Z\s\t\n_-]")
 # matches any sequence of tabs, newlines, spaces, underscores, and dashes
 spaceMatcher = re.compile(r"[\t\n\s_-]+")
 # matches for special wiki words like '(disambiguation)'
@@ -28,7 +28,6 @@ slashMatcher = re.compile(r"[.\r|.\n|.\t]")
 # matches for special parts of url
 urlMatcher = re.compile(r"https|http|www|com|org|edu")
 
-r'(?<=[a-zA-z])(?P<punc>[.!?])(?=\s[A-Z])'
 
 ## Funcitons ##
 def clean_text(rawString):
@@ -39,7 +38,7 @@ def clean_text(rawString):
     # replace spaceMatcher and tagMatcher with " "
     detaggedString = re.sub(tagMatcher, " ", rawString)
     # converts anything that looks like a year range (eg. 1910-11) into two years (eg. 1910 1911)
-    rangedString = re.sub(r'\s(?P<firstTwo>[0-9]{2})(?P<secondTwo>[0-9]{2})-(?P<lastTwo>[0-9]{2})', "\g<firstTwo>\g<secondTwo> \g<firstTwo>\g<lastTwo>", detaggedString)
+    rangedString = re.sub(r'\b(?P<firstTwo>[0-9]{2})(?P<secondTwo>[0-9]{2})-(?P<lastTwo>[0-9]{2})', "\g<firstTwo>\g<secondTwo> \g<firstTwo>\g<lastTwo>", detaggedString)
     # replace stripMatcher with ""
     cleanedString = re.sub(stripMatcher, "", rangedString)
     # replace spaceMatcher with " " and strip surround whitespace
