9e2392c4b014032d68a08987b9cd89fd18f2b793
landjbs <landonsmith@college.harvard.edu>
2019-06-20 12:55:04 -0400

    levenshteinGenerator init

---
+++ backend/searchers/fuzzyMatcher.py
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+typename T::size_type LevensteinDistance(const T &source, const T &target) {
+    if (source.size() > target.size()) {
+        return LevensteinDistance(target, source);
+    }
+
+    using TSizeType = typename T::size_type;
+    const TSizeType min_size = source.size(), max_size = target.size();
+    std::vector<TSizeType> lev_dist(min_size + 1);
+
+    for (TSizeType i = 0; i <= min_size; ++i) {
+        lev_dist[i] = i;
+    }
+
+    for (TSizeType j = 1; j <= max_size; ++j) {
+        TSizeType previous_diagonal = lev_dist[0], previous_diagonal_save;
+        ++lev_dist[0];
+
+        for (TSizeType i = 1; i <= min_size; ++i) {
+            previous_diagonal_save = lev_dist[i];
+            if (source[i - 1] == target[j - 1]) {
+                lev_dist[i] = previous_diagonal;
+            } else {
+                lev_dist[i] = std::min(std::min(lev_dist[i - 1], lev_dist[i]), previous_diagonal) + 1;
+            }
+            previous_diagonal = previous_diagonal_save;
+        }
+    }
+
+    return lev_dist[min_size];
+}
