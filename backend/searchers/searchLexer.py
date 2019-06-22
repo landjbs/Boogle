9eb1f4bc1f86f464f16bf92f48ecad5fc8e782a9
landjbs <landonsmith@college.harvard.edu>
2019-06-22 14:05:48 -0400

    topSearch init

--- backend/searchers/searchLexer.py
+++ backend/searchers/searchLexer.py
@@ -1,1 +1,8 @@
 import searchers.databaseSearcher as databaseSearcher
+
+def topSearch(rawSearch):
+    """
+    Highest level search analyzer that takes in a raw search and decides
+    which search function to employ.
+    """
+    
