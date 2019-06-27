dd1b93670e82053bd1caf09059d12d385e720c58
landjbs <landonsmith@college.harvard.edu>
2019-06-27 16:57:40 -0400

    modelTrainer REPL test

--- backend/modelTrainer.py
+++ backend/modelTrainer.py
@@ -11,10 +11,10 @@ import os
 import pandas as pd
 from dataStructures.objectSaver import save, load
 from models.processing.cleaner import clean_text
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Activation
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
+from termcolor import colored
+import numpy as np
 
 from bert_serving.client import BertClient
 bc = BertClient()
@@ -33,15 +33,16 @@ def encode_folder(folderPath, folderNum, n):
         with open(f'{folderPath}/{file}', 'r') as fileObj:
             try:
                 text = fileObj.read()
-                vec = bc.encode([text)[0]
-                vecDict = dv.docVec_to_dict(vec)
-                vecDict = {0:0}
-                vecDict.update({'folder':folderNum})
+                vec = bc.encode([text])[0]
+                vecDict = {file:vec}
+                # vecDict = dv.docVec_to_dict(vec)
+                # vecDict = {0:0}
+                # vecDict.update({'folder':folderNum})
                 folderList.append(vecDict)
             except Exception as e:
                 print(e)
             print(f'\t{i}', end="\r")
-            print('\n')
+    print('\n')
     return folderList
 
 
@@ -54,15 +55,39 @@ def encode_folderList(topFolderPath):
             print(folder)
             folderPath = f'{topFolderPath}/{folder}'
             if folder == 'Business':
-                vecList += encode_folder(folderPath, 1, 24111)
+                vecList += encode_folder(folderPath, 1, 3) #24111
             else:
-                vecList += encode_folder(folderPath, 0, 1854)
+                vecList += encode_folder(folderPath, 0, 3) #1854
     return vecList
 
-# vecDict = encode_folderList(PATH)
-#
-# vecDF = pd.DataFrame(vecDict)
+vecList = encode_folderList(PATH)
+
+docVecs = {}
+
+for elt in vecList:
+    docVecs.update(elt)
+
+def score_doc(queryVec, docVec):
+    score = np.sum(queryVec * docVec) / np.linalg.norm(docVec)
+    return score
+
+while True:
+    query = input(colored('IN: ', 'red'))
+
+    queryVec = bc.encode([query])[0]
+
+    scoredDocs = [(doc, score_doc(queryVec, docVecs[doc])) for doc in docVecs]
+
+    scoredDocs.sort(key=(lambda elt : elt[-1]), reverse=True)
+
+    for i, doc in enumerate(scoredDocs):
+        print(colored(doc[1], 'blue'), end='> ')
+        print(colored(doc[0], 'cyan'), end='\n')
+        if i > 5:
+            break
+
+# vecDF = pd.DataFrame(vecList)
 #
 # vecDF.to_csv('data/outData/binning/trainingVecsBERT.csv')
 
-vecDF = pd.read_csv('data/outData/binning/trainingVecsBERT.csv', sep=',')
+# vecDF = pd.read_csv('data/outData/binning/trainingVecsBERT.csv', sep=',')
