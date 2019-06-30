979668bb323dda1e3ec28f78e2d102b368e2dabc
landjbs <landonsmith@college.harvard.edu>
2019-06-30 17:17:20 +0000

    vectorize_masked_tokens imp

--- backend/test.py
+++ backend/test.py
@@ -30,19 +30,60 @@ scoreDict = {}
 maskToken = '<MASK>'
 
 for token in foundTokens:
-    print(colored(f'\t{token}', 'red'), end=' | ')
-    tempDoc = re.sub(token, maskToken, document)
-    print(f'\t{tempDoc}')
+
     tempVec = bc.encode([tempDoc])[0]
     dist = euclidean(baseVec, tempVec)
-    print(colored(dist, 'green'))
+
     scoreDict.update({token:dist})
 
-plt.bar(scoreDict.keys(), scoreDict.values())
-plt.ylabel('Euclidean Distance from Base Vector')
-plt.title(f'Tokens Iteratively Replaced With "{maskToken}"')
-plt.show()
 
-# def score_doc(queryVec, docVec):
-#     score = np.sum(queryVec * docVec) / np.linalg.norm(docVec)
-#     return score
+
+def vectorize_masked_tokens(document, maskToken='', knowledgeProcessor=None, scoringMethod='euclidean', disp=False):
+    """
+    Iteratively masks all knowledge tokens in document string and determines
+    score relative to initial vector. Returns dict of token and score
+        -document: string of the document to vectorize
+        -maskToken: string to replace each token with
+        -knowledgeProcessor: knowledge tokens to locate, will be built from document if not given
+        -scoringMethod: use euclidean distance or dot product to determine distance from baseVec
+        -disp: whether to display the bar chat of distances
+    """
+    # assertions and special conditions
+    assert isinstance(document, str), "document must have type 'str'"
+    assert isinstance(maskToken, str), "maskToken must have type 'str'"
+    assert (scoringMethod in ['euclidean', 'dot']), "scoringMethod must be 'euclidean' or 'dot'"
+
+    if not knowledgeProcessor:
+        knowledgeProcessor = build_knowledgeProcessor(document.split())
+
+    # define scoring method
+    if (scoringMethod=='euclidean'):
+        def calc_score(maskedVec, baseVec):
+            return euclidean(maskedVec, baseVec)
+    elif (scoringMethod=='dot'):
+        def calc_score(maskedVec, baseVec):
+            return np.sum(maskedVec * baseVec) / np.linalg.norm(baseVec)
+
+    # calculate vector of raw document
+    baseVec = bc.encode([document])[0]
+
+    # find tokens in document with both greedy and non-greedy matching
+    foundTokens = find_rawTokens(document, knowledgeProcessor)
+
+    scoreDict = {}
+
+    for token in foundTokens:
+        print(colored(f'\t{token}', 'red'), end=' | ')
+        maskedDoc = re.sub(token, maskToken, document)
+        maskedVec = bc.encode([maskedDoc])[0]
+        score = calc_score(maskedVec, baseVec)
+        print(colored(dist, 'green'))
+        scoreDict.update({token:score})
+
+    if disp:
+        plt.bar(scoreDict.keys(), scoreDict.values())
+        plt.ylabel('Euclidean Distance from Base Vector')
+        plt.title(f'Tokens Iteratively Replaced With "{maskToken}"')
+        plt.show()
+
+    return scoreDict
