32a8af3fc97db0a9eef4a68c22efc1f9375e2b9a
landjbs <landonsmith@college.harvard.edu>
2019-07-11 15:27:37 -0400

    wikiReader init

--- backend/test.py
+++ backend/test.py
@@ -1,34 +1,14 @@
-import re
-import os
-import numpy as np
-import pandas as pd
-from termcolor import colored
-import matplotlib.pyplot as plt
-from scipy.spatial.distance import euclidean
-from scipy.spatial.distance import cosine, euclidean
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.decomposition import PCA
+# from models.knowledge.knowledgeFinder import find_scoredTokens
 
-from dataStructures.objectSaver import load
-# from models.knowledge.knowledgeFinder import find_rawTokens
-# from models.ranking.distributionRanker import rank_distribution
-# from models.knowledge.knowledgeBuilder import build_knowledgeProcessor
-# import models.binning.docVecs as docVecs
+filePath = 'data/inData/wikipedia_utf8_filtered_20pageviews.csv'
 
-import models.binning.bertAnalytics as bertAnalytics
-print('imported')
-
-
-while True:
-    doc = input('BERT Expression: ')
-    print(bertAnalytics.bert_multiParser(doc))
-
-# while True:
-#     new = input("search: ")
-#     newVec = docVecs.vectorize_doc(new)
-#     scoreLambda = lambda tokenTuple : (euclidean(tokenTuple[0], newVec), tokenTuple[1])
-#     scoredDocs = list(map(scoreLambda, vecList))
-#     scoredDocs.sort()
-#     print('\tRelated Searches:')
-#     for tokenTuple in scoredDocs:
-#         print(f"\t{tokenTuple[1]}: {tokenTuple[0]}")
+with open(filePath, 'r') as wikiCsv:
+    for i, line in enumerate(wikiCsv):
+        if i < 100:
+            # get everything after the first comma
+            rawText = line[21:]
+            titleEnd = rawText.find('  ')
+            title = rawText[:titleEnd]
+            articleText = rawText[(titleEnd+2):]
+            articleText = articleText.strip()
+            divDict = {}
