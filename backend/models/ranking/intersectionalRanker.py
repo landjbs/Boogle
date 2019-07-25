2899e40f1a9f772182777083cef3ee339ba82470
landjbs <landonsmith@college.harvard.edu>
2019-07-25 15:00:28 +0100

    score_vectorized_intersection init

--- backend/models/ranking/intersectionalRanker.py
+++ backend/models/ranking/intersectionalRanker.py
@@ -1,3 +1,5 @@
+from scipy.spatial.distance import euclidean
+
 def score_simple_intersection(pageObj, tokenWeights):
     """
     Scores page by baseScore and score of multiple tokens
@@ -15,3 +17,12 @@ def score_simple_intersection(pageObj, tokenWeights):
     aggregateScore = baseScore + knowledgeScore
 
     return aggregateScore
+
+def score_vectorized_intersection(pageObj, tokenWeights, searchVec):
+    """
+    Scores page by baseScore, score of multiple tokens, and relationship
+    with search vector
+    """
+    baseScore = pageObj.baseScores
+    pageTokens = pageObj.knowledgeTokens
+    pageVec =
