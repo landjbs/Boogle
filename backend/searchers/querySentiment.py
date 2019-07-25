0e7b80c2d1cdfaf3629b368113ca21cb82a0539d
landjbs <landonsmith@college.harvard.edu>
2019-07-25 14:00:09 +0100

    querySentiment lambda progress

--- backend/searchers/querySentiment.py
+++ backend/searchers/querySentiment.py
@@ -10,9 +10,9 @@ from math import exp
 from collections import Counter
 # from models.binning.tokenScoring import get_masked_weights
 
-# TO DO::: HAND TUNE SIGMOID ACT[pIVATION FUNCTION
+# TO DO::: HAND TUNE SIGMOID ACTIVATION FUNCTION
 # calc_score_activation = lambda freq : (exp(freq) / (exp(freq) + 1))
-calc_score_activation = lambda freq : freq
+calc_score_activation = lambda freq : exp(freq) / (exp(freq) + 1)
 
 def score_token_importance(rawSearch, tokenSet, freqDict):
     """
@@ -21,9 +21,9 @@ def score_token_importance(rawSearch, tokenSet, freqDict):
     """
     # maskedWeights = Counter(get_masked_weights(tokenSet, rawSearch))
     # tokens are ranked with td-idf schema using freqDict: tokenScore = inverseDocumentFreq * termFreq
-    tokenScores = {token:(calc_score_activation(1/freqDict[token][0])) for token in tokenSet}
+    tokenScores = {token:(calc_score_activation(freqDict[token][0])
+                    for token in tokenSet}
     # numPoints = sum([tokenScores[token] for token in tokenScores])
-    # print(numPoints)
     # tokenScores = dict(map(lambda elt : (tokenScores[elt] / numPoints), tokenScores))
     # print(tokenScores)
     return tokenScores
