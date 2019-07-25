63273e24f3ffbfeeae6c832aea9ccaacf19750e4
landjbs <landonsmith@college.harvard.edu>
2019-07-25 14:05:11 +0100

    simple baseRanker imp

--- backend/dataStructures/pageObj.py
+++ backend/dataStructures/pageObj.py
@@ -22,6 +22,17 @@ class Page():
         self.videoScore =       pageDict['videoScore']
         self.windowText =       pageDict['windowText']
 
+    def __init__(self, pageDict):
+        """ Initialize page object with pageList of features """
+        self.url =              pageDict['url']
+        self.title =            pageDict['title']
+        self.knowledgeTokens =  pageDict['knowledgeTokens']
+        self.pageVec =          pageDict['pageVec']
+        self.linkList =         pageDict['linkList']
+        self.loadDate =         pageDict['loadDate']
+        self.baseScore =        pageDict['baseScore']
+        self.windowText =       pageDict['windowText']
+
     def display(self, tokenList):
         """
         Returns the parts of the page to be displayed as search results
--- backend/models/ranking/baseRanker.py
+++ backend/models/ranking/baseRanker.py
@@ -1,10 +1,11 @@
 loadLambda = lambda loadtime: loadTime
 
-def calc_base_score(loadTime):
+def calc_base_score(baseAttributes):
     """
     Establishes base score for page during crawl to avoid
     storing page attributes that won't change and aren't
     necessary for searching
     """
+    loadTime = baseAttributes['loadTime']
     base_score = loadLambda(loadTime)
     return base_score
