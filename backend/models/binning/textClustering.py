77b2381d24681b48e213f42398319898b6734961
landjbs <landonsmith@college.harvard.edu>
2019-07-25 18:06:52 +0100

    textClustering people analysis

--- backend/models/binning/textClustering.py
+++ backend/models/binning/textClustering.py
@@ -1,13 +1,20 @@
+import numpy as np
 from os import listdir
 
 from dataStructures.objectSaver import load
 
-def cluster_file_contents(filePath):
-    vecDict = {}
+people = ['Luther Dickinson', 'T. Coraghessan Boyle', 'Georges Parfait Mbida Messi',
+            'Jim Bohannon', 'Paula Raymond']
 
+peopleVecs = []
+
+def cluster_file_contents(filePath, n):
+    vecDict = {}
     for i, file in enumerate(listdir(filePath)):
+        if i > n:
+            break
         pageList = load(f'{filePath}/{file}')
         for pageDict in pageList:
+            if pageDict['title'].strip() in people:
+                peopleVecs.append(pageDict['pageVec'])
             vecDict.update({pageDict['title']: pageDict['pageVec']})
-
-            print(vecDict)
--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,3 @@
 from models.binning.textClustering import cluster_file_contents
 
-cluster_file_contents(filePath='data/thicctable/wikiCrawl4')
+cluster_file_contents(filePath='data/thicctable/wikiCrawl4', n=10000)
