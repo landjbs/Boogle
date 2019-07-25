caf9be0d4936155e21162e49b3c6bf4d2f19b54d
landjbs <landonsmith@college.harvard.edu>
2019-07-25 14:11:45 +0100

    sort_score imp

--- backend/models/ranking/pageRanker.py
+++ backend/models/ranking/pageRanker.py
@@ -1,33 +1,24 @@
+"""
+Scores single page at sort time as a function of its baseScore, tokenScores,
+and any special intersections with the bucket.
+"""
+
+
 # scoring lambdas that map page-related scalars onto functions
 # function to penalize long load times
 loadLambda = lambda loadTime : loadTime
 # function to normalize aggregateScore of page to avoid huge outliers NO IMP YET!
 normalizationLambda = lambda aggregateScore : aggregateScore
 
-## Load ML Models ##
-# from models.binning.docVecs import docVec_to_dict
-# from keras.models import load_model
-# import pandas as pd
-# binaryModel = load_model('data/outData/binning/binaryModel.sav')
-####################
 
-
-def score_single(pageObj, token):
+def sort_score(pageObj, token):
     """
     Ranks pageObj based on attributes
     """
-
     # pull out the token-specific score assigned by knowledgeFinder
     tokenScore = pageObj.knowledgeTokens[token]
-    # score page based on loading speed
-    loadPenalty = loadLambda(pageObj.loadTime)
-
-    ### Test ranker to score news higher STILL UNDER CONSTRUCTION ###
-    # dfVec = pd.DataFrame([docVec_to_dict(pageObj.pageVec)])
-    # print(dfVec)
-    # newsScore = (binaryModel.predict(dfVec))[1]
-    # newsBooster = 4 if (newsScore > 0.4) else 0
-    #################################################################
+    # pull out the base score of the page
+    baseScore = pageObj.baseScore
 
     aggregateScore = tokenScore - loadPenalty
     normalizedScore = normalizationLambda(aggregateScore)
