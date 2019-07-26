5d0cb4491452028d04449998837b0e5c6900795c
landjbs <landonsmith@college.harvard.edu>
2019-07-26 15:58:26 +0100

    scramble_fake_queries

--- backend/models/knowledge/dependencyParser.py
+++ backend/models/knowledge/dependencyParser.py
@@ -1,15 +1,25 @@
 import spacy
+import textacy.extract
 
 # Load the large English NLP model
 nlp = spacy.load('en_core_web_lg')
 
 # The text we want to examine
-text = """London is the capital and most populous city of England and
-the United Kingdom.  Standing on the River Thames in the south east
-of the island of Great Britain, London has been a major settlement
-for two millennia. It was founded by the Romans, who named it Londinium.
+text = """London is the capital and most populous city of England and  the United Kingdom.
+Standing on the River Thames in the south east of the island of Great Britain,
+London has been a major settlement  for two millennia.  It was founded by the Romans,
+who named it Londinium.
 """
 
-def get_entities(text):
-    doc = nlp(text)
-    return [(entity.text, entity.label_) for entity in doc.ents]
+# Parse the document with spaCy
+doc = nlp(text)
+
+# Extract semi-structured statements
+statements = textacy.extract.semistructured_statements(doc, "London")
+
+# Print the results
+print("Here are the things I know about London:")
+
+for statement in statements:
+    subject, verb, fact = statement
+    print(f" - {fact}")
