df4432ec1f0b10afe15029b0673429ac38c6f223
Landon Smith <landjbs@gmail.com>
2019-08-19 15:06:50 -0400

    modified corrDict reading imp

---
+++ backend/models/knowledge/shadowTokenization.py
@@ -0,0 +1,21 @@
+"""
+I define a 'shadow token' as a knowledgeToken that does not occur on a given
+page, but who still has a high ability to describe the contents of the page
+as indicated by the tokens and sentiment that are explicityly present on the
+page. This module attempts to add relevent shadowTokens to the knowlegeTokens
+of a page during preprocessing/crawling.
+"""
+
+from collections import Counter
+
+def add_shadow_tokens(knowledgeTokens, relationshipDict):
+    """
+    Adds shadow tokens to the knowledgeTokens of page
+    Args:
+        knowledgeTokens:        Dict of knowledgeTokens and scores from
+                                    knowledgeFinder processing.
+        relationshipDict:       Hashmap from tokens to tokens with high
+                                    related-ness and generated in
+                                    knowledgeRelations.
+    """
+    relatedCounts = Counter()
--- backend/test.py
+++ backend/test.py
@@ -1,90 +1,21 @@
-from time import time
-import numpy as np
-import matplotlib.pyplot as plt
+from os import listdir
+from numpy import dot
+from tqdm import tqdm
 
-from collections import Counter
 from dataStructures.objectSaver import load
-from models.processing.cleaner import clean_text
-from models.knowledge.knowledgeBuilder import (build_token_relationships,
-                                                build_corr_dict,
-                                                build_knowledgeProcessor,
-                                                vector_update_corrDict)
-from models.knowledge.knowledgeFinder import score_divDict
+from models.knowledge.knowledgeNetwork import build_corr_dict
+from models.knowledge.knowledgeBuilder import vector_update_corrDict
 
-# knowledgeProcessor = load('data/outData/knowledge/knowledgeProcessor.sav')
+
+wikiPath = 'data/thicctable/wikiCrawl_SHADOW_NOVECS'
+
 freqDict = load('data/outData/knowledge/freqDict.sav')
 
-title = 'harvard university'
-all = 'harvard university is a liberal arts university in cambridge massachusets. it has 16000 undergraduates and 20000 associates. famous alumni include boogle founder, landon smith, and facebook founder, mark zuckerberg.'
+with open('data/inData/commonWords.txt', 'r') as wordsFile:
+    tokenFreqs = {line.strip():(freqDict[line.strip()]) for line in tqdm(wordsFile)
+                    if line.strip() in freqDict}
+print(len(tokenFreqs))
 
-specialSet = {'harvard university', 'liberal arts', 'liberal arts university',
-            'cambridge massachusets', 'mark zuckerberg', 'landon smith'}
-for word in (title.split() + all.split()):
-    specialSet.add(word)
-knowledgeProcessor = build_knowledgeProcessor(specialSet)
-
-while True:
-    divDict = {'title':title, 'all':all}
-    print(score_divDict(divDict, knowledgeProcessor, freqDict))
-    title = input('title: ')
-    all = input('all: ')
-    words = set(title.split() + all.split())
-    # knowledgeProcessor = build_knowledgeProcessor(words)
-
-# establish paths
-FREQ_PATH = 'data/outData/knowledge/freqDict.sav'
-WIKI_PATH = 'data/inData/wikipedia_utf8_filtered_20pageviews.csv'
-
-
-# # load freqDict and choose some sub selection if desired
-# freqDict = load(FREQ_PATH)
-# freqKeys = [token for token in freqDict.keys()]
-#
-# myTokens = ['computer game', 'video game', 'play station', 'gameboy', 'speakers',
-#             'television', 'arcade', 'xbox', 'radio', 'music', 'programming',
-#             'coding', 'store', 'invention', 'microwave', 'refrigerator',
-#             'python', 'java', 'programming language', 'violence', 'concern',
-#             'parents', 'children', 'kids', 'developer', 'battle', 'knight',
-#             'film', 'movie', 'critic', 'rotten tomatoes', 'pixar', 'film studio',
-#             'animated short', 'best', 'favorite', 'top', 'coffee',
-#             'starbucks', 'dunkin donuts', 'knight', 'fortress', 'castle',
-#             'frappe', 'espresso', 'wood', 'log', 'cabin', 'forest', 'axe',
-#             'tree', 'chainsaw', 'forester', 'logging', 'catapult', 'crosbow',
-#             'arrow', 'fire', 'forest']
-#
-# myFreqs = {token : freqDict[token]
-#             for token in myTokens if token in freqDict}
-#
-# corrDict = build_corr_dict(filePath=WIKI_PATH, freqDict=myFreqs,
-#                     outPath='data/outData/knowledge/corrDict.sav')
-#
-# print(corrDict)
-
-# corrDict = load('data/outData/knowledge/corrDict.sav')
-
-while True:
-    s = input('S: ')
-    if s == '//':
-        break
-    try:
-        print(f'\t{s}')
-        for elt in corrDict[s][:5]:
-            print(f'\t\t{elt[0]}> {elt[1]}')
-    except Exception as e:
-        print(f'ERROR: {e}')
-
-
-vecCorrDict = vector_update_corrDict(WIKI_PATH, corrDict, corrWeight=0.6,
-    tokenWeight=0.2, textWeight=0.2,
-    outPath='data/outData/knowledge/relationshipDict.sav')
-
-while True:
-    s = input('S: ')
-    if s == '//':
-        break
-    try:
-        print(f'\t{s}')
-        for elt in vecCorrDict[s]:
-            print(f'\t\t<{elt[0]}> {elt[1]}')
-    except Exception as e:
-        print(f'ERROR: {e}')
+corrDict = (build_corr_dict('data/thicctable/wikiCrawl_SHADOW_NOVECS',
+                freqDict=tokenFreqs,
+                outPath='data/outData/knowledge/corrDict_NEW.sav'))
