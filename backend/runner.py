1c9ca6dcb720eb3328bb7c3210aeabc697f06c12
Landon Smith <landjbs@gmail.com>
2019-09-08 19:45:30 -0400

    runner rm

--- backend/runner.py
+++
@@ -1,4 +0,0 @@
-from searchers.modelBuilders.questionAnsweringModel import train_answering_lstm
-
-train_answering_lstm('data/outData/searchAnalysis/squadDataFrames',
-                    outPath='answerModel.h5')
